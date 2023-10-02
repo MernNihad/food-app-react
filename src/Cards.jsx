7c06884b78b62c3557b82106313791c29771267e
nihadot <mysonisfunny@gmail.com>
2023-10-02 11:38:12 +0530

    intial commit

---
+++ src/Cards.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+function Cards(props) {
+    console.log(props)
+
+
+    const { image,para,title  } = props
+
+  return (
+    <div className='card'>
+        <div className="container">
+            <img src={image}  alt="" />
+        </div>
+        <div className="title">
+            <h2>{title}</h2>
+            <p>{para}</p>
+        </div>
+    
+    </div>
+  )
+}
+
+export default Cards
